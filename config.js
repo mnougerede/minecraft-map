d6235518680d31a5202455ff43148ac470d78183
mnougerede <mnougerede@gmail.com>
2026-02-09 12:34:32 +0000

    moved /public files to root for github pages

---
+++ config.js
@@ -0,0 +1,5 @@
+// Optional Supabase config.
+// Leave empty to use data.json instead.
+// Consider gitignoring this file if you store real credentials here.
+window.SUPABASE_URL = "https://ozirmefpmdoqrmxzkalg.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_9En3jnBR281fG5bSVpTMiA_vp7AG41r";
